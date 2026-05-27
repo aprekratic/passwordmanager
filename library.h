eed558ddd96c7624bbf27a78f868024e7adb3488
aprekratic <andrijana.prekratic1@gmail.com>
2026-05-27 09:18:47 +0200

    Update library.h

--- library.h
+++ library.h
@@ -18,43 +18,41 @@
 
 extern int brojUnosa;
 
-
 typedef enum {
-MENU_VIEW_ALL = 1,
-MENU_VIEW_ONE = 2,
-MENU_ADD = 3,
-MENU_DELETE = 4,
-MENU_GENERATE = 5,
-MENU_EXIT = 6,
+	MENU_VIEW_ALL = 1,
+	MENU_VIEW_ONE = 2,
+	MENU_ADD = 3,
+	MENU_DELETE = 4,
+	MENU_GENERATE = 5,
+	MENU_EXIT = 6,
 } MenuOption;
 
 typedef struct {
-char site[MAX_SITE_LEN];
-char username[MAX_USER_LEN];
-char password[MAX_PASS_LEN];
+	char site[MAX_SITE_LEN];
+	char username[MAX_USER_LEN];
+	char password[MAX_PASS_LEN];
 } Entry;
 
 typedef struct {
-Entry* entries;
-int count;
-int capacity;
+	Entry* entries;
+	int count;
+	int capacity;
 } Vault;
 
-
+void clearBuff(void);
 
 static inline void pressEnter(void)
 {
-    printf("\nPritisni enter za nastavak...");
-    clearBuff();
+	printf("\nPritisni enter za nastavak...");
+	clearBuff();
 }
 
 Vault* vaultCreate(void);
 void vaultPrintAll(const Vault* v);
 int authSetup(void);
 int authLogin(void);
-void clearBuff(void);
 int storageSave(const Vault* v);
-void menuMain(Vault *v);
+void menuMain(Vault* v);
 void vaultDestroy(Vault* v);
 int vaultAdd(Vault* v, const char* site, const char* user, const char* pass);
 
